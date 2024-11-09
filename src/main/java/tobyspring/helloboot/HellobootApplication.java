942495874e14657cbf904959f352096b26e16f76
kyh1126 <kyh901126@nate.com>
2024-11-09 22:40:20 +0900

    [4. 독립 실행형 서블릿 애플리케이션] 서블릿 등록

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,20 +1,40 @@
 package tobyspring.helloboot;
 
-import org.apache.catalina.startup.Tomcat;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 //@SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 //		SpringApplication.run(HellobootApplication.class, args);
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
-		webServer.start();
-	}
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.addServlet("hello", new HttpServlet() {
+                    @Override
+                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                        super.service(req, resp);
+                        resp.setStatus(200);
+                        resp.setHeader("Content-Type", "text/plain");
+                        resp.getWriter().println("Hello Servlet");
+                    }
+                }).addMapping("/hello");
+            }
+
+
+        });
+        webServer.start();
+    }
 
 }
