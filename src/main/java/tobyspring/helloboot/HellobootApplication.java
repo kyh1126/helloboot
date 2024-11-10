af67d80b11250f7102399861a75814f230492792
kyh1126 <kyh901126@nate.com>
2024-11-10 19:52:49 +0900

    [4. 독립 실행형 서블릿 애플리케이션] 서블릿 요청 처리 - 리팩토링

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,10 +2,11 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,19 +19,17 @@ public class HellobootApplication {
     public static void main(String[] args) {
 //		SpringApplication.run(HellobootApplication.class, args);
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
-            @Override
-            public void onStartup(ServletContext servletContext) throws ServletException {
-                servletContext.addServlet("hello", new HttpServlet() {
-                    @Override
-                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello",
+                    new HttpServlet() {
+                        @Override
+                        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //                        super.service(req, resp);
-                        resp.setStatus(200);
-                        resp.setHeader("Content-Type", "text/plain");
-                        resp.getWriter().println("Hello Servlet");
-                    }
-                }).addMapping("/hello");
-            }
+                            resp.setStatus(HttpStatus.OK.value());
+                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                            resp.getWriter().println("Hello Servlet");
+                        }
+                    }).addMapping("/hello");
 
 
         });
