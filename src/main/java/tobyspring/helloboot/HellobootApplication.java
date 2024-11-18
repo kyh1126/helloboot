a89152723fa3b7018f65dec58c9a0c491a01c4ff
kyh1126 <kyh901126@nate.com>
2024-11-18 20:43:09 +0900

    [5. 독립 실행형 스프링 애플리케이션] 스프링 컨테이너 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,8 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,10 +19,12 @@ import java.io.IOException;
 public class HellobootApplication {
 
     public static void main(String[] args) {
-//		SpringApplication.run(HellobootApplication.class, args);
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
 
             servletContext.addServlet("frontcontroller",
                     new HttpServlet() {
@@ -30,6 +34,7 @@ public class HellobootApplication {
                             if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                                 String name = req.getParameter("name");
 
+                                HelloController helloController = applicationContext.getBean(HelloController.class);
                                 String ret = helloController.hello(name);
 
                                 resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
