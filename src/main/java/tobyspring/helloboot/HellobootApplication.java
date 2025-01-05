ba9a3112c7094e55b061c443fe03da21ebbbdb2b
kyh1126 <kyh901126@nate.com>
2025-01-06 04:06:47 +0900

    [11. 스프링 부트 자세히 살펴보기] 자동 구성 조건 결과 확인

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,7 +1,10 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.annotation.PostConstruct;
@@ -12,6 +15,21 @@ public class HellobootApplication {
 
     public HellobootApplication(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Bean
+    ApplicationRunner run(ConditionEvaluationReport report) {
+        return args -> {
+            System.out.println(report.getConditionAndOutcomesBySource().entrySet().stream()
+                    .filter(co -> co.getValue().isFullMatch())
+                    .filter(co -> co.getKey().indexOf("Jmx") < 0)
+                    .map(co -> {
+                        System.out.println(co.getKey());
+                        co.getValue().forEach(c -> System.out.println("\t" + c.getOutcome()));
+                        System.out.println();
+                        return co;
+                    }).count());
+        };
     }
 
     @PostConstruct
